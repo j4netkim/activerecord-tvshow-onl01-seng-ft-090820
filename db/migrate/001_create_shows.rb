895763ca9c5626ac8904880a8da1e9878d4b0577
J K <jkim65@buffalo.edu>
2020-10-13 18:56:53 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,6 +3,8 @@ class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
       t.string :name
+      t.string :network
+      t.string :day
       t.
   end
 end
